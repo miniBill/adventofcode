e60143a04b6652ad211850273c72fbaa3a11f4cd
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2018-12-11 22:20:39 +0100

    More compact

--- 1/1.hs
+++ 1/1.hs
@@ -1,16 +1,5 @@
-import System.IO (isEOF)
-import Text.Read (readMaybe)
-
-inputFold :: Show a => (String -> a -> a) -> a -> IO ()
-inputFold fold seed =
-  let step i = do
-        eof <- isEOF
-        if eof
-          then return i
-          else do
-            l <- getLine
-            step (fold l i)
-   in step seed >>= print
+inputFold :: (String -> a -> a) -> a -> IO a
+inputFold fold seed = foldr fold seed . lines <$> readFile "input.txt"
 
 main :: IO ()
 main =
@@ -18,4 +7,4 @@ main =
         case l of
           ('+':ls) -> a + (read ls :: Integer)
           _ -> a + (read l :: Integer)
-   in inputFold step 0
+   in inputFold step 0 >>= print
